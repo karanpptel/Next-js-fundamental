da7978a68b513f859f062fcf9b2d9d588b53bc75
karanpptel <2203031457010@paruluniversity.ac.in>
2025-08-07 19:01:01 +0530

    final commit

--- src/app/photo-feed/layout.tsx
+++ src/app/photo-feed/layout.tsx
@@ -1,11 +1,11 @@
 import "./styles.css";
 
 export default function Layout(props: {
-    children: React.ReactNode,
-    model: React.ReactNode
+    children: React.ReactNode, // will be a page or nested layout
+    model: React.ReactNode // this is parallel Model route :(@model) this is special route cuz it enables simultensious rendering
 }) {
     return <>
         {props.model}
-        {props.children}
+        {props.children} 
     </>
 }
